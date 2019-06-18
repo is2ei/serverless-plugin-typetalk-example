e209dbdb4c0294094979da0d956a59f3f36bd033
Issei Horie <is2ei.horie@gmail.com>
2019-06-19 01:28:25 +0900

    Removed unused comments

--- handler.js
+++ handler.js
@@ -8,7 +8,4 @@ module.exports.hello = async (event) => {
       input: event,
     }, null, 2),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
